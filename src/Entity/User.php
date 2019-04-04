91c6d021e3a2129a643668fc46d6c20baa6fc5d9
Joaden <chanet.denis@hotmail.fr>
2019-04-04 13:53:10 +0200

    feat: fosuserbundle

---
+++ src/Entity/User.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
